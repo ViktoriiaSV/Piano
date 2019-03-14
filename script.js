dd9dcb1bbd00b1e6f10cb139d8c8553bc143b8d0
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2019-03-14 15:28:31 +0200

    created piano

---
+++ script.js
@@ -0,0 +1,42 @@
+let a = document.getElementById("piano-a");
+
+function playAudioA() {
+    a.play();
+}
+
+let s = document.getElementById("piano-s");
+
+function playAudioS() {
+    s.play();
+}
+
+let d = document.getElementById("piano-d");
+
+function playAudioD() {
+    d.play();
+}
+let f = document.getElementById("piano-f");
+
+function playAudioF() {
+    f.play();
+}
+let g = document.getElementById("piano-g");
+
+function playAudioG() {
+    g.play();
+}
+let h = document.getElementById("piano-h");
+
+function playAudioH() {
+    h.play();
+}
+let j = document.getElementById("piano-j");
+
+function playAudioJ() {
+    j.play();
+}
+let k = document.getElementById("piano-k");
+
+function playAudioK() {
+    k.play();
+}
